843cec418b480699dc16bed649248d173645be27
Nguyễn Đức Cường <cuongnd.hmstech@gmail.com>
2026-08-14 22:53:49 +0700

    update vietnamese word

--- src/main/java/org/example/doansummer2026/repository/StaffScheduleRepository.java
+++ src/main/java/org/example/doansummer2026/repository/StaffScheduleRepository.java
@@ -6,10 +6,8 @@ import org.example.doansummer2026.model.StaffInfo;
 import org.example.doansummer2026.model.StaffSchedule;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
